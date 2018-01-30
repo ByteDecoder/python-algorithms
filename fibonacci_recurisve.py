1a22db80c0a1ffd9138bc865912e59308e0d0aca
Rodrigo Reyes <rreyes@canaryhealth.com>
2018-01-29 23:50:09 -0600

    Update fibonacci_recurisve.py

--- fibonacci_recurisve.py
+++ fibonacci_recurisve.py
@@ -1,3 +1,12 @@
+# Adding memoization to improve the function performance
+def memoize(f):
+    memo = {}
+    def helper(x):
+        if x not in memo:            
+            memo[x] = f(x)
+        return memo[x]
+    return helper
+    
 def get_fib(n):
     if n == 0:
         return 0
@@ -5,3 +14,6 @@ def get_fib(n):
         return 1
     else:
         return get_fib(n-1) + get_fib(n-2)
+
+# Decorting the function with memoization
+get_fib = memoize(get_fib)
